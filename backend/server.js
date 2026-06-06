3e9241175f4adc8b74c03470d8ce5630583ca018
Mittal2801 <mituprajapati2801@gmail.com>
2026-06-06 12:14:32 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://food-delivery-frontend.onrender.com",
+    origin: "https://food-delivery-frontend-iqq6.onrender.com",
     credentials: true,
   })
 );
