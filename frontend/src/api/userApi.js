194d7296e1b2b7588144a6d376e957ebf1fc68da
Mittal2801 <mituprajapati2801@gmail.com>
2026-06-06 12:51:57 +0530

    add

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,17 +1,29 @@
+import axios from "axios";
+
 const API = "https://food-delivery-0p1u.onrender.com";
 
+// register
 export const registerUser = async (userData) => {
   const res = await axios.post(
     `${API}/api/users/register`,
     userData
   );
+
   return res.data;
 };
 
+// login
 export const loginUser = async (userData) => {
   const res = await axios.post(
-    `${API}/api/users/login`,
+    "https://food-delivery-0p1u.onrender.com/api/users/login",
     userData
   );
+
+  return res.data;
+};
+
+// get users
+export const getUsers = async () => {
+  const res = await axios.get(API);
   return res.data;
 };
